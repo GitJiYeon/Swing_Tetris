e64e5d5738e542e7f6c8071c619a8727b5f8965b
한지연 <s2436@e-mirim.hs.kr>
2024-11-12 03:45:23 +0900

    설정UI 개선, 모드 구현

--- src/SwingTetris/TetrisJFrame.java
+++ src/SwingTetris/TetrisJFrame.java
@@ -1,19 +1,27 @@
 package SwingTetris;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class TetrisJFrame extends JFrame {
     public TetrisJFrame() {
-        setTitle("Tetris");
-        setSize(600, 800);
+        setTitle("Tetris - made by Jiyeon");
+        setSize(650, 850);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //패널 추가
+        // 패널 추가
         TetrisPanel panel = new TetrisPanel();
-        add(panel);
+        panel.setPreferredSize(new java.awt.Dimension(650, 800)); // 패널의 크기를 설정
+        add(panel); // 패널 추가
+
+        pack(); // JFrame의 크기를 패널에 맞게 조정
         setVisible(true);
-        //포커스 ㅐ=ㅍㄶ
+
+        // 포커스 요청
         panel.requestFocusInWindow();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new TetrisJFrame());
     }
 }
